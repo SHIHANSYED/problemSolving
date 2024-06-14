1ef0e905163be752fba812ee05fa356762ca1e3a
SHIHANSYED <shihansyedali@gmai.com>
2024-06-14 11:37:40 +0530

    question added

--- src/sumOfDigits.java
+++ src/sumOfDigits.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
+
+//Given a number, N. Find the sum of all the digits of N
+//
+//
+//Example 1:
+//
+//Input:
+//N = 12
+//Output:
+//        3
+//Explanation:
+//Sum of 12's digits:
+//        1 + 2 = 3
+
 public class sumOfDigits {
     public static void main(String[] args) {
 
