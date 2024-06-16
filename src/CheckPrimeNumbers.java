853c3d8a4232ff849089949a089b077bc85eee03
SHIHANSYED <shihansyedali@gmai.com>
2024-06-16 17:13:17 +0530

    changes

--- src/CheckPrimeNumbers.java
+++ src/CheckPrimeNumbers.java
@@ -2,20 +2,16 @@ import java.util.Scanner;
 
 public class CheckPrimeNumbers {
     public static void main(String[] args) {
-        System.out.println("Enter the limit");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter thr num");
         int num = sc.nextInt();
-
-        if (num == 1)
-            System.out.println("No " + num);
+        if (num == 1) System.out.println("Not prime " + num);
         for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
-                System.out.println("No " + num);
-                return;
-            }
+            if (num % i == 0)
+                System.out.println("Not prime " + num);
 
         }
-        System.out.println("Yes " + num);
+        System.out.println("prime " + num);
 
     }
 }
